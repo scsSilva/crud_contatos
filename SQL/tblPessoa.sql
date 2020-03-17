d6517b98eb4120704043085f2c7cba2dc9cbc95c
Sallys Carlos <53477221+scsSilva@users.noreply.github.com>
2020-03-17 09:46:28 -0300

    Update tblPessoa.sql

--- SQL/tblPessoa.sql
+++ SQL/tblPessoa.sql
@@ -1,5 +1,5 @@
 create table if not exists tblPessoa(
-    id int not null primary key,
+    id int not null primary key auto_increment,
     nome varchar(100) not null,
     email varchar(45) not null,
     telefone varchar(20) not null
