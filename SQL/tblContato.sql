8eeafcb23cd92b143b187a19aa6719f4247d3e4c
Sallys Carlos <53477221+scsSilva@users.noreply.github.com>
2020-03-19 14:10:06 -0300

    Update and rename tblPessoa.sql to tblContato.sql

--- SQL/tblContato.sql
+++ SQL/tblContato.sql
@@ -1,4 +1,4 @@
-create table if not exists tblPessoa(
+create table if not exists tblContato(
     id int not null primary key auto_increment,
     nome varchar(100) not null,
     email varchar(45) not null,
